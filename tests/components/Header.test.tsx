41ca496b63c78e2bf5b28233b195c369ffdb12f2
Malicia <a.b@c.com>
2025-07-25 10:36:30 -0400

    Improve website title by displaying my name and occupation

--- tests/components/Header.test.tsx
+++ tests/components/Header.test.tsx
@@ -3,12 +3,23 @@ import { render, screen } from '@testing-library/react';
 import Header from '../../src/components/Header';
 
 describe('Header', () => {
-	it('should display the given title as a heading', () => {
-		const title = 'Given title';
-		render(<Header title={title} />);
+	function renderComponent() {
+		render(<Header />);
+	}
 
-		const heading = screen.getByRole('heading');
+	it('should display my name', () => {
+		renderComponent();
 
-		expect(heading).toHaveTextContent(title);
+		const headerTitle = screen.getByRole('heading');
+
+		expect(headerTitle).toHaveTextContent('Marie Darrigol');
+	});
+
+	it('should display my occupation', () => {
+		renderComponent();
+
+		const headerSubtitle = screen.getByRole('doc-subtitle');
+
+		expect(headerSubtitle).toHaveTextContent('Développeuse web');
 	});
 });
