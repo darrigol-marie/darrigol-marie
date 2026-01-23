2c7d830a6090b9dc10ab712a28591179de9920da
Malicia <a.b@c.com>
2026-01-23 12:06:39 -0500

    Simplify LoadingScreen component + update its tests

--- tests/components/LoadingScreen.test.tsx
+++ tests/components/LoadingScreen.test.tsx
@@ -1,56 +1,29 @@
-import {
-	render,
-	screen,
-	waitForElementToBeRemoved,
-} from '@testing-library/react';
-import {
-	QueryClient,
-	QueryClientProvider,
-	type UseQueryOptions,
-} from '@tanstack/react-query';
-import { http, HttpResponse } from 'msw';
+import { render, screen } from '@testing-library/react';
 
-import LoadingScreen from '../../src/components/LoadingScreen';
-import { server } from '../mocks/server';
+import LoadingScreen, {
+	type LoadingScreenProps,
+} from '../../src/components/LoadingScreen';
 
 describe('LoadingScreen', () => {
-	const mockupPath = '/loading';
-	const queryOptions: UseQueryOptions = {
-		queryKey: ['loading'], // TODO: see if we can add a queryFn value without breaking the test
-	};
-
-	function renderComponent() {
-		return render(<LoadingScreen query={queryOptions} />, {
-			wrapper: ({ children }) => {
-				return (
-					<QueryClientProvider
-						client={
-							new QueryClient({ defaultOptions: { queries: { retry: false } } })
-						}
-					>
-						{children}
-					</QueryClientProvider>
-				);
-			},
-		});
+	function renderComponent({ isLoading, isError }: LoadingScreenProps) {
+		render(<LoadingScreen isLoading={isLoading} isError={isError} />);
 	}
 
+	// TODO: to replace by an animation with a clear tag (to ensure accessibility and that we can select the animation for tests)
 	it('should display a message while data are loading', () => {
-		renderComponent();
+		renderComponent({ isLoading: true, isError: false });
 
 		expect(screen.getByText(/chargement/i)).toBeInTheDocument();
 	});
 
-	it('should remove the loading message once data are loaded', async () => {
-		renderComponent();
+	it('should remove the loading message when data are loaded', async () => {
+		renderComponent({ isLoading: false, isError: false });
 
-		await waitForElementToBeRemoved(() => screen.getByText(/chargement/i));
+		expect(screen.queryByText(/chargement/i)).not.toBeInTheDocument();
 	});
 
 	it('should display a message if an error occured during data fetching', async () => {
-		server.use(http.get(mockupPath, () => HttpResponse.error()));
-
-		renderComponent();
+		renderComponent({ isLoading: true, isError: true });
 
 		expect(await screen.findByText(/erreur/i)).toBeInTheDocument();
 	});
