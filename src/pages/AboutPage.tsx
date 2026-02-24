10ec92271dcb82df322ee0f8a8b0e262687a2027
Malicia <a.b@c.com>
2026-02-24 14:06:08 -0500

    Update About page text

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,16 +1,21 @@
 function AboutPage() {
 	return (
 		<>
-			<p>Bonjour, bonsoir à vous !</p>
+			<p>Bienvenue à vous !</p>
 			<p>
-				Je m'appelle Marie, je suis développeuse web spécialisée en front-end et
-				écrivaine à mes heures perdues.
+				Je m’appelle Marie, je suis développeuse web et écrivaine. J’ai plus de cinq
+				ans d’expérience dans le développement front-end avec TypeScript et des
+				connaissances en développement back-end. Ma formation tourne essentiellement
+				autour de l’informatique, les jeux vidéo et les arts graphiques, mais j’ai
+				également suivi un certificat en création littéraire.
 			</p>
 			<p>
-				Vous trouverez sur ce site un aperçu de mon parcours professionnel, mes
-				projets personnels et mon univers artistique.
+				Vous trouverez sur ce site un aperçu de mon parcours professionnel, de mes
+				projets personnels et de mon univers artistique. C’est une vitrine que j’ai
+				mise en place pour présenter ce que je fais, mais également pour m’offrir un
+				espace d’expérimentation.
 			</p>
-			<p>Bonne visite !</p>
+			<p>Merci de m’avoir lue et bonne visite !</p>
 		</>
 	);
 }
