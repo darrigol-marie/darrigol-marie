00a9c49d0be171531a83479ea2af3456bda39e05
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:56:58 -0400

    Add Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom';
+
+import './Navigation.css';
+
+import type { AppRoute } from '../App.routes';
+
+interface Props {
+	links: AppRoute[];
+}
+
+function Navigation({ links }: Props) {
+	return (
+		<nav>
+			<ul>
+				{links.map((link) => {
+					return (
+						<li>
+							<NavLink to={link.path}>{link.name}</NavLink>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+}
+
+export default Navigation;
