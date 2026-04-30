892ca30df4e11afafe9d35300027444cfad6daa6
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:09 -0400

    Add date to Post component + add test

--- tests/components/Post.test.tsx
+++ tests/components/Post.test.tsx
@@ -5,7 +5,6 @@ import Post from '../../src/components/Post';
 /**
  * Les tests à écrire :
  *
- * - il faut afficher une date
  * - il faut afficher un sous-titre si on en a un
  * - il faut afficher le contenu [comment découper ça ?]
  * - il faut afficher un lien si on en a un
@@ -14,10 +13,20 @@ describe('Post', () => {
 	it('should display a title', () => {
 		const postTitle = 'Title';
 
-		render(<Post title={postTitle} />);
+		render(<Post title={postTitle} date={'XXX'} />);
 		const postElement = screen.getByRole('heading');
 
 		expect(postElement).toBeInTheDocument();
 		expect(postElement).toHaveTextContent(postTitle);
+	});
+
+	it('should display a date', () => {
+		const postDate = '202X';
+
+		render(<Post title={'Title'} date={postDate} />);
+		const postElement = screen.getByRole('time');
+
+		expect(postElement).toBeInTheDocument();
+		expect(postElement).toHaveTextContent(postDate);
 	});
 });
