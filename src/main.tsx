43f608adf393b9f0167e120586e99b23bdb6a08d
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:13 -0400

    Fix 404 error on page reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import './index.scss';
 import { rootRoute } from './App.routes.tsx';
 
 axios.defaults.baseURL = import.meta.env.VITE_DATA_PATH;
 
-const router = createBrowserRouter([rootRoute]);
+const router = createHashRouter([rootRoute]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
