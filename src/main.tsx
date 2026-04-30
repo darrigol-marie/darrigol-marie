68f61b911c93365f572cd8f1aa699211e5525a94
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:56:57 -0400

    Add routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 
 import App from './App.tsx';
-import { sitePages } from './types/page.type.tsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App pages={sitePages} />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
