78ebda9e69b42b1c096826ea31c66f1be2a36b1b
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:15 -0400

    Refactoring for posts fetching on Experience page

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,41 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-
 import PostsList from '../components/PostsList';
 import LoadingScreen from '../components/LoadingScreen';
-
-export interface Experience {
-	id: string;
-	date: string;
-	position: string;
-	company: string;
-	description: string[];
-}
+import { usePosts } from '../hooks/usePosts';
 
 function ExperiencePage() {
-	const {
-		data = [],
-		isLoading,
-		isError,
-	} = useQuery({
-		queryKey: ['experience'],
-		queryFn: () =>
-			axios.get('/experiences.json').then((response) => response.data),
-	});
+	const { data = [], isLoading, isError } = usePosts();
 
 	return (
 		<LoadingScreen isLoading={isLoading} isError={isError}>
-			<PostsList
-				posts={data.map((experience: Experience) => {
-					return {
-						id: experience.id,
-						title: experience.position,
-						subtitle: experience.company,
-						date: experience.date,
-						paragraphs: experience.description,
-					};
-				})}
-			/>
+			<PostsList posts={data} />
 		</LoadingScreen>
 	);
 }
